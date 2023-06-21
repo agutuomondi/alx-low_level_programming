5082940e5277e294694ca27f3f7039a5efa2ca4c
agutuomondi <agutudennis1997@gmail.com>
2023-06-21 07:33:07 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print - prints 10 times the alphabet, in lowercase
+ * Return: void
+ */
+
+void print_alphabet_x10(void);
+{
+	char ch;
+	int i
+		i = 0;
+	while (i < 10)
+	
+		for (ch = 'a'; ch <= 'z' ch++)
+	putchar (ch);
+	putchar('\n');
+	i++
+}
+
