f121a024186927582b70b24e9939f9d737b4f396
agutuomondi <agutudennis1997@gmail.com>
2023-06-28 01:56:28 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,21 +8,17 @@
 int main(void)
 
 {
-	int i = 'o';
-
-	while (i <= '9');
+	int i = '0';
+	while (i <= '9')
 	{
 		putchar(i);
-
 		if (i != '9')
 		{
 			putchar(',');
-
 			putchar(',');
 		}
 		++i;
 	}
 	putchar('\n');
-
 	return (0);
 }
