9a75bbe37ce60bac4d3fd39d2c2ad04891792eb1
agutuomondi <agutudennis1997@gmail.com>
2023-07-11 07:44:33 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,25 +16,28 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
+
 	while (s1[a])
 		a++;
+
 	while (s2[b])
 		b++;
+
 	d = a + b;
 	s = malloc((sizeof(char) * d) + 1);
+
 	if (s == NULL)
-	{
 		return (NULL);
-		b = 0;
-	}
+	b = 0;
+
 	while (c < d)
 	{
 		if (c <= a)
-		{
 			s[c] = s1[c];
-		}
+
 		if (c >= a)
 		{
 			s[c] = s2[b];
