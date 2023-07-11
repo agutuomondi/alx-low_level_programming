0a1c97c7d878f6cd0190495c6a5537a61e2c5e54
agutuomondi <agutudennis1997@gmail.com>
2023-07-11 14:39:43 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
+
+/**
+ * word_len - index making.
+ * @str: string to be searched.
+ * Return 0
+ */
+
+int word_len(char *str)
+{
+	int index = 0, len = 0;
+
+	while (*(str + index) && *((str + index) != ' ')
+	{
+	
+		l++;
+		i++;
+	}
+	return (l)
+	
+
