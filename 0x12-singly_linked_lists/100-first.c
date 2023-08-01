8561bd6785abdbfaca3c8712e8364caded37f147
agutuomondi <agutudennis1997@gmail.com>
2023-08-01 07:11:54 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,9 +7,15 @@ void first(void) __attribute__ ((constructor));
   *  function is executed.
   */
 
+
+void first(void) __attribute__((constructor));
+
+/**
+ * first - prints a sentence before main.
+ */
+
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-
 	printf("I bore my house upon my back!\n");
 }
