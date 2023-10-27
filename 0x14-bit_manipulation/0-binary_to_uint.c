9c6cacdf29e4073e18d83073b40eb66fa8b805c7
aagutuomondi <mwalimuagutu@gmail.com>
2023-10-27 08:38:33 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,24 +8,24 @@
  */
 
 unsigned int binary_to_uint(const char *b)
-	unsigned int n;
-	int len, b;
+{
+	unsigned int num;
+	int i;
 
 	if (!b)
 	return (0);
-	n = 0;
+	num = 0;
 
-	for (len = 0; b[len] != '\0'; len++)
-	for (len--, b = 1; len >= 0; lem--, b *= 2)
-{
-	if (b[len] != '0' && b[len] != '1')
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		return (0);
+		if (b[i] != '0' && b[i] != '1')
+			return (0)
 	}
-	if (b[len] & 1)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		n += b;
+		num << = 1;
+		if (b[i]== '1')
+			num +=1;
 	}
-
+	return (num)
 }
-return (n);
