fa1aef50ebd9c52be18554c16367eb7741245ca5
aagutuomondi <mwalimuagutu@gmail.com>
2023-10-31 12:56:02 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <fcnt1.h>
-#include <sys/atat.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 /**
  * read_textfile  - reads the text file and prints letter
@@ -18,27 +18,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t nrd, nwr;
 	char *buffer;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-	{
-		close(fd);
 		return (0);
-	}
+
 	nrd = read(fd, buffer, letters);
+	nwr = write(STDOUT_FILENO, buffer, nrd);
+
 	close(fd);
-	if (nrd == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-	nwr = write(STDOUT_FILENO, buffer, ndr);
 	free(buffer);
-	if (nrd != nwr)
-		return (0);
+
 	return (nwr);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <fcnt1.h>
-#include <sys/atat.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
