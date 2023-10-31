d368ffb7a4e3aa190b826f54e73ae270a4c11242
aagutuomondi <mwalimuagutu@gmail.com>
2023-10-31 12:08:26 -0700

    vim 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,4 @@
 #include "main.h"
-
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -18,28 +16,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t nrd, nwr;
-	char *buf;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-	nrd = read(fd, buf, letters);
+	nrd = read(fd, buffer, letters);
 	close(fd);
 	if (nrd == -1)
 	{
-		free(buf);
+		free(buffer);
 		return (0);
 	}
-	nwr = write(STDOUT_FILENO, buf, ndr);
-	free(buf);
+	nwr = write(STDOUT_FILENO, buffer, ndr);
+	free(buffer);
 	if (nrd != nwr)
 		return (0);
 	return (nwr);
