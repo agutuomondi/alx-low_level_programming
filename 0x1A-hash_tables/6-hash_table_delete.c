bc2ee5bc3575e0b6fd0dd374bff0488c285055ab
agutuomondi <mwalimuagutu@gmail.com>
2023-12-22 12:42:26 -0800

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,24 +6,23 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    hash_node_t *node, *tmp;
-    unsigned long int i;
+	hash_node_t *node, *tmp;
+	unsigned long int i;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        for (node = ht->array[i]; node != NULL;)
-        {
-            tmp = node->next;
-            free(node->key);
-            free(node->value);
-            free(node);
-            node = tmp;
-        }
-    }
-
-    free(ht->array);
-    free(ht);
+	for (i = 0; i < ht->size; i++)
+	{
+		for (node = ht->array[i]; node != NULL;)
+		{
+			tmp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = tmp;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
