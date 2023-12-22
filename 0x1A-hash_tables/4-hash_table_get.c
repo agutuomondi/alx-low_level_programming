fead148136bbfda2840f178aa659d01161a73ac6
agutuomondi <mwalimuagutu@gmail.com>
2023-12-22 12:33:51 -0800

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,19 +11,17 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    hash_node_t *node;
-    unsigned long int index;
+	hash_node_t *node;
+	unsigned long int index;
 
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (NULL);
-
-    index = key_index((const unsigned char *)key, ht->size);
-
-    if (index >= ht->size)
-        return (NULL);
-
-    for (node = ht->array[index]; node != NULL && strcmp(node->key, key) != 0; node = node->next)
-        ;
-
-    return (node == NULL ? NULL : node->value);
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+	for (node = ht->array[index];
+			node != NULL && strcmp(node->key, key) != 0;
+			node = node->next)
+		;
+	return (node == NULL ? NULL : node->value);
 }
