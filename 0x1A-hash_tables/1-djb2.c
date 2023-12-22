0949fd056afb4768a75d4098b68d0fc9a3c92391
agutuomondi <mwalimuagutu@gmail.com>
2023-12-22 12:12:46 -0800

    1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -8,10 +8,9 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash = 5381;
+	unsigned long int hash = 5381;
 
-    for (; *str != '\0'; str++)
-        hash = ((hash << 5) + hash) + (*str);
-
-    return hash;
+	for (; *str != '\0'; str++)
+		hash = ((hash << 5) + hash) + (*str);
+	return (hash);
 }
