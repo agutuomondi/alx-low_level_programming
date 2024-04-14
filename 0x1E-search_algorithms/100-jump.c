723b1cf7ff6fc2a9f18a7f0e5e4f9c3a3c992925
agutuomondi <mwalimuagutu@gmail.com>
2024-04-14 04:53:21 -0700

    100-jump.c

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,47 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - search algorithm
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of the elements in array
+ * @value: Value to search
+ * Return: Index where value is located, or -1 if not present
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	int block;
+	int block_size;
+	int i = 0;
+	int holder = 0;
+
+	if (!array || size == 0)
+		return (-1);
+	block = sqrt((int)size);
+	block_size = block;
+
+	printf("Value checked array[%d] = [%d]\n", holder, array[holder]);
+
+	while (array[block] <= value && block <= (int)size - 1)
+	{
+		if (array[i] == value || array[block] == value)
+			break;
+		holder = block;
+		printf("Value checked array[%d] = [%d]\n", holder, array[block]);
+		block = block + block_size;
+	}
+
+	printf("Value found between indexes [%d] and [%d]\n", holder, block);
+
+	while (holder <= block)
+	{
+		if (holder >= (int)size)
+			return (-1);
+		printf("Value checked array[%d] = [%d]\n", holder, array[holder]);
+		if (array[holder] == value)
+			return (holder);
+		holder++;
+	}
+
+	return (-1);
+}
